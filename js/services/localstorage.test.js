eca55eefcca386e87a1c1e2e09dd0e05c30e9c97
cherrykot <kot.aleksei43@gmail.com>
2019-11-28 15:32:14 +0200

    JBI-12: + new test for localstorage

--- js/services/localstorage.test.js
+++ js/services/localstorage.test.js
@@ -4,10 +4,14 @@ import {
 } from '../constants/common.constants';
 
 describe('localStorage', () => {
+  const nodeStore = new LocalStorage();
+  
   it('should return null if can\'t take getItem(key)', () => {
-    const nodeStore = new LocalStorage();
-
     expect(nodeStore.getItem('Key')).toBe(null);
     expect(nodeStore.getItem(TRANSPORT_LIST_STORAGE_KEY)).toBe(null);
+  });
+
+  it('should be prefix', () => {
+    expect(nodeStore.prefix).toBe('JS-Band-');
   });
 });
